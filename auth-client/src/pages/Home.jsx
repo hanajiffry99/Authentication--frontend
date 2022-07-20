fb78b630b3e6fddd681c70188384915815ee63ae
Hana Jiffry <hanajiffryscg@gmail.com>
2022-07-20 17:02:44 +0530

    Login Page UI

--- auth-client/src/pages/Home.jsx
+++ auth-client/src/pages/Home.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Grid } from "@mui/material";
 
-import {Button } from '@mui/material';
+import "./styles.css";
 
 const Home = () => {
   return (
     <>
       <h1>Home Page</h1>
-      <Link to ="/register">Register</Link>
-      <Link to ="/login">Login</Link>
+      <Grid container spacing={2}>
+        <Grid item xs={9}></Grid>
+
+        <Grid item xs={3}>
+          <span style={{paddingRight:'10%'}}>
+            <Button variant="contained" color="info">
+              <Link to="/register" className="links">
+                Register
+              </Link>
+            </Button>
+          </span>
+
+          <span>
+            <Button variant="contained" color="secondary">
+              <Link to="/login" className="links">
+                Login
+              </Link>
+            </Button>
+          </span>
+        </Grid>
+      </Grid>
     </>
-  )
-}
+  );
+};
 
 export default Home;
--- auth-client/src/pages/Users/LoginUsers.jsx
+++ auth-client/src/pages/Users/LoginUsers.jsx
@@ -1,22 +1,114 @@
-import React from 'react';
-import { useNavigate,Link} from 'react-router-dom';
+import { Button, FormLabel, TextField } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const LoginUsers = () => {
-
   const history = useNavigate();
 
-  const handleSubmit = ()=>{
-    history("/dashboard",{replace: true});
-  }
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  const [FormErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setFormErrors(validate(values));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(FormErrors).length === 0 && isSubmit) {
+      console.log("success 2");
+      LoginUser();
+    }
+  }, [FormErrors]);
+
+  const LoginUser = () => {
+    alert("success");
+    history("/dashboard", { replace: true });
+  };
 
   return (
     <>
-      <h1>Login</h1>
-      <button onClick={handleSubmit}>Submit</button>
-      <div>Still no Account</div>
-      <Link to ="/register">Create One</Link>
-    </>
-  )
-}
+      <div className="formPage">
+        <div className="FormContainer">
+          <form onSubmit={handleSubmit}>
+            <div className="formTitle">Login</div>
+            <FormLabel required className="label">
+              Email
+            </FormLabel>
+            <TextField
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              error={FormErrors.email}
+              helperText={FormErrors.email}
+              style={{ paddingBottom: "30px" }}
+              fullWidth
+            />
 
-export default LoginUsers
+            <FormLabel required className="label">
+              Password
+            </FormLabel>
+            <TextField
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              error={FormErrors.password}
+              helperText={FormErrors.password}
+              style={{ paddingBottom: "30px" }}
+              fullWidth
+            />
+
+            <span style={{ paddingLeft: "87%" }}>
+              <Button type="submit" variant="contained" color="secondary">
+                Login
+              </Button>
+            </span>
+          </form>
+
+          <br />
+          <br />
+          <div className="title">Still no Account !!</div>
+          <span style={{ paddingLeft: "40%" }}>
+            <Button variant="outlined" color="info">
+              <Link to="/register" className="links" style={{ color: "blue" }}>
+                Create One
+              </Link>
+            </Button>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginUsers;
